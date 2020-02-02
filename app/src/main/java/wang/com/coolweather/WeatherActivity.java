e4c54a735d6c4127fa665d8ccdef52aaf563e67c
hymister <1131400780@qq.com>
2020-02-02 20:33:57 +0800

    增加后台自动更新天气的功能

--- app/src/main/java/wang/com/coolweather/WeatherActivity.java
+++ app/src/main/java/wang/com/coolweather/WeatherActivity.java
@@ -5,6 +5,7 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -30,6 +31,7 @@ import okhttp3.Callback;
 import okhttp3.Response;
 import wang.com.coolweather.gson.Forecast;
 import wang.com.coolweather.gson.Weather;
+import wang.com.coolweather.service.AutoUpdateService;
 import wang.com.coolweather.util.HttpUtil;
 import wang.com.coolweather.util.Utility;
 
@@ -188,6 +190,8 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 
     private void loadBingPic() {
